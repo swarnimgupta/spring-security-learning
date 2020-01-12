9938ff79424474f08edebe3d9252016c5fc6df5a
Swarnim Gupta <swarnim94@gmail.com>
2020-01-12 14:50:01 +0530

    ResponseToUser.java added for response json

---
+++ src/main/java/io/sam/ssl/models/ResponseToUser.java
@@ -0,0 +1,8 @@
+package io.sam.ssl.models;
+
+import lombok.Data;
+
+@Data
+public class ResponseToUser {
+    private final String response;
+}
--- src/main/java/io/sam/ssl/resources/Controller.java
+++ src/main/java/io/sam/ssl/resources/Controller.java
@@ -1,22 +1,28 @@
 package io.sam.ssl.resources;
 
+import io.sam.ssl.models.ResponseToUser;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class Controller {
     @GetMapping("/")
-    public String welcome() {
-        return "<h1>Welcome</h1>";
+    public ResponseToUser welcome() {
+        log.info("Accessing normal welcome method for expose");
+        return new ResponseToUser("Welcome");
     }
 
     @GetMapping("/user")
-    public String welcomeUser() {
-        return "<h1>Welcome User</h1>";
+    public ResponseToUser welcomeUser() {
+        log.info("Accessing user welcome method for expose");
+        return new ResponseToUser("Welcome User");
     }
 
     @GetMapping(value = "/admin")
-    public String welcomeAdmin() {
-        return "<h1>Welcome Admin</h1>";
+    public ResponseToUser welcomeAdmin() {
+        log.info("Accessing admin welcome method for expose");
+        return new ResponseToUser("Welcome Admin");
     }
 }
